f985d9b16cb04362a9bf2aa3711f452c9bd1c463
imran-khan-dev <emrankhanniloy@gmail.com>
2025-10-03 19:20:04 +0600

    Refactor: Usertoken code

--- src/app/utils/userTokens.ts
+++ src/app/utils/userTokens.ts
@@ -1,71 +1,67 @@
-// import httpStatus from "http-status-codes";
-// import { JwtPayload } from "jsonwebtoken";
-// import { envVars } from "../config/env";
-// import { IsActive, IUser } from "../modules/user/user.interface";
-// import { generateToken, verifyToken } from "./jwt";
-// import { User } from "../modules/user/user.model";
-// import AppError from "../errorHelpers/AppError";
+import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
+import { envVars } from "../config/env";
+import { generateToken, verifyToken } from "./jwt";
+import AppError from "../errorHelpers/AppError";
+import { User } from '@prisma/client';
+import { prisma } from "../../db";
 
-// export const createUserTokens = (user: Partial<IUser>) => {
-//   const jwtPayload = {
-//     userId: user._id,
-//     email: user.email,
-//     role: user.role,
-//   };
-//   const accessToken = generateToken(
-//     jwtPayload,
-//     envVars.JWT_ACCESS_SECRET,
-//     envVars.JWT_ACCESS_EXPIRES
-//   );
+export const createUserTokens = (user: User) => {
+    const jwtPayload = {
+        userId: user.id,
+        email: user.email,
+        role: user.role,
+    };
+    const accessToken = generateToken(
+        jwtPayload,
+        envVars.JWT_ACCESS_SECRET,
+        envVars.JWT_ACCESS_EXPIRES
+    );
 
-//   const refreshToken = generateToken(
-//     jwtPayload,
-//     envVars.JWT_REFRESH_SECRET,
-//     envVars.JWT_REFRESH_EXPIRES
-//   );
+    const refreshToken = generateToken(
+        jwtPayload,
+        envVars.JWT_REFRESH_SECRET,
+        envVars.JWT_REFRESH_EXPIRES
+    );
 
-//   return {
-//     accessToken,
-//     refreshToken,
-//   };
-// };
+    return {
+        accessToken,
+        refreshToken,
+    };
+};
 
-// export const createNewAccessTokenWithRefreshToken = async (
-//   refreshToken: string
-// ) => {
-//   const verifiedRefreshToken = verifyToken(
-//     refreshToken,
-//     envVars.JWT_REFRESH_SECRET
-//   ) as JwtPayload;
 
-//   const isUserExist = await User.findOne({ email: verifiedRefreshToken.email });
+export const createNewAccessTokenWithRefreshToken = async (
+    refreshToken: string
+) => {
 
-//   if (!isUserExist) {
-//     throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
-//   }
-//   if (
-//     isUserExist.isActive === IsActive.BLOCKED ||
-//     isUserExist.isActive === IsActive.INACTIVE
-//   ) {
-//     throw new AppError(
-//       httpStatus.BAD_REQUEST,
-//       `User is ${isUserExist.isActive}`
-//     );
-//   }
-//   if (isUserExist.isDeleted) {
-//     throw new AppError(httpStatus.BAD_REQUEST, "User is deleted");
-//   }
+    
+    const verifiedRefreshToken = verifyToken(
+        refreshToken,
+        envVars.JWT_REFRESH_SECRET
+    ) as JwtPayload;
 
-//   const jwtPayload = {
-//     userId: isUserExist._id,
-//     email: isUserExist.email,
-//     role: isUserExist.role,
-//   };
-//   const accessToken = generateToken(
-//     jwtPayload,
-//     envVars.JWT_ACCESS_SECRET,
-//     envVars.JWT_ACCESS_EXPIRES
-//   );
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            email: verifiedRefreshToken.email
+        }
+    });
 
-//   return accessToken;
-// };
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+    }
+
+
+    const jwtPayload = {
+        userId: isUserExist.id,
+        email: isUserExist.email,
+        role: isUserExist.role,
+    };
+    const accessToken = generateToken(
+        jwtPayload,
+        envVars.JWT_ACCESS_SECRET,
+        envVars.JWT_ACCESS_EXPIRES
+    );
+
+    return accessToken;
+};
