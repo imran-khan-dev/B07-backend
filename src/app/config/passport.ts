eda0d051cc1e1657a08ecd4757eece48cdf6b1f0
imran-khan-dev <emrankhanniloy@gmail.com>
2025-10-03 20:38:51 +0600

    Refactor: Passport js with prisma

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,65 +1,63 @@
-// import bcryptjs from "bcryptjs";
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import passport from "passport";
-// import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { prisma } from "../../db";
 
-// import { IsActive } from "../modules/user/user.interface";
-// import { User } from "../modules/user/user.model";
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "password",
+        },
+        async (email: string, password: string, done) => {
+            try {
+                const isUserExist = await prisma.user.findUnique({
+                    where: {
+                        email: email
+                    }
+                });
 
-// passport.use(
-//   new LocalStrategy(
-//     {
-//       usernameField: "email",
-//       passwordField: "password",
-//     },
-//     async (email: string, password: string, done) => {
-//       try {
-//         const isUserExist = await User.findOne({ email });
-
-//         if (!isUserExist) {
-//           return done("User does not exist");
-//         }
-
-//         if (
-//           isUserExist.isActive === IsActive.BLOCKED ||
-//           isUserExist.isActive === IsActive.INACTIVE
-//         ) {
-//           return done(`User is ${isUserExist.isActive}`);
-//         }
-
-//         if (isUserExist.isDeleted) {  
-//           return done("User is deleted"); 
-//         }
+                if (!isUserExist) {
+                    return done("User does not exist");
+                }
 
 
-//         const isPasswordMatched = await bcryptjs.compare(
-//           password as string,
-//           isUserExist.password as string
-//         );
+                const isPasswordMatched = await bcryptjs.compare(
+                    password as string,
+                    isUserExist.passwordHash as string
+                );
 
-//         if (!isPasswordMatched) {
-//           return done(null, false, { message: "Password does not match" });
-//         }
+                if (!isPasswordMatched) {
+                    return done(null, false, { message: "Password does not match" });
+                }
 
-//         return done(null, isUserExist);
-//       } catch (error) {
-//         console.log(error);
-//         done(error);
-//       }
-//     }
-//   )
-// );
+                return done(null, isUserExist);
+            } catch (error) {
+                console.log(error);
+                done(error);
+            }
+        }
+    )
+);
 
-// passport.serializeUser((user: any, done: (err: any, id?: unknown) => void) => {
-//   done(null, user._id);
-// });
+passport.serializeUser((user: any, done: (err: any, id?: unknown) => void) => {
+    done(null, user._id);
+});
 
-// passport.deserializeUser(async (id: string, done: any) => {
-//   try {
-//     const user = await User.findById(id);
-//     done(null, user);
-//   } catch (error) {
-//     console.log(error);
-//     done(error);
-//   }
-// });
+passport.deserializeUser(async (id: string, done: any) => {
+
+    const userIdNum = Number(id)
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userIdNum
+            }
+        });
+        done(null, user);
+    } catch (error) {
+        console.log(error);
+        done(error);
+    }
+});
